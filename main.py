48db526d8bee40b480f2682487ece7fe9b3c9fcf
iiVeil <52388052+iiVeil@users.noreply.github.com>
2023-07-26 06:24:24 -0400

    done

--- main.py
+++ main.py
@@ -1,69 +1,109 @@
 import curses
-from math import floor
 from pyTermUI.ui import UI
 from pyTermUI.region import Region
 from pyTermUI.position import Position
 from pyTermUI.text import Text
-
+from pyTermUI.Toolkit.asciiart import AsciiArt
+from random import randint
 
 def main(stdscr):
+    
+    turn = 0
     def on_click(text):
-        ...
+        nonlocal turn
+        cell_id = text.region.data.get("id")
+        
+        if cells[cell_id] != None:
+            return
+        cells[cell_id] = turn
+        turn = not turn
+        
+        update_grid()
 
+        ui.draw()
+        
+    def update_grid():
+        
+        for cell in cells:
+            if cells[cell] is not None:
+                center = regions[cell-1].size.half() + regions[cell-1].start
+                arts[cell-1] =  AsciiArt(states[cells[cell]], mainRegion, center)
+                arts[cell-1].create()
+            
+        check_win()
+                
+    def check_win():
+        wins = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,7]
+        ]
+        for win in wins:
+            if cells[win[0]] == None or cells[win[1]] == None or cells[win[2]] == None:
+                continue
+            if (cells[win[0]] == cells[win[1]] == cells[win[2]]):
+                print(f"{'O' if cells[win[0]] else 'X'} WINS!")
+                ui.deactivate()
+                
+                
+             
+                
+        
     ui = UI(stdscr)
 
-    region = Region("Test", Position.ORIGIN(), Position.DEFAULT_TERM_SIZE())
-    region.framed = True
+    mainRegion = Region("Test", Position.ORIGIN(), Position.DEFAULT_TERM_SIZE())
+    mainRegion.framed = False
 
-    cellwidth = 18
+    cellwidth = 16
     cellheight = 6
 
-    cells = []
+    states = ["\    /\n \  / \n  \/  \n  /\  \n /  \ \n/    \\", "╭────╮\n│    │\n│    │\n│    │\n│    │\n╰────╯"]
 
-    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    arts = [None, None, None, None, None, None, None, None, None]
+    board = "                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                \n────────────────┼────────────────┼────────────────\n                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                \n────────────────┼────────────────┼────────────────\n                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                \n                │                │                "
+    art = AsciiArt(board, mainRegion, mainRegion.size.half())
+    
+    cells = {
+        1: None, 2: None, 3: None,
+        4: None, 5: None, 6: None,
+        7: None, 8: None, 9: None
+        }
+    
+            
+    
+    art.create()
+    
+            
+    regions = []
+    
+    for y in range(3):
+        for x in range(3):
+            region = Region(f"{x+1*y+1}", art.startpos + (Position((cellwidth*x+x), (cellheight*y+y)) + Position(1,1)), Position(cellwidth, cellheight))
+            region.data = {"id": len(regions)+1}
+            region.framed = False
+            regions.append(region)
+    
+    for region in regions:
+        for y in range(region.size.y+1):
+            text = Text("S"*region.size.x, Position(0-1, y-1))
+            text.callback = on_click
+            region.add_element(text)
+    
+    
+    for region in regions:
+        ui.add_region(region)
+    
 
-    vertical = "│"
-    horizontal = "─"
-    cross = "┼"
+    
 
-    boardx = cellwidth * 2 + 2
-    boardy = cellheight * 2 + 2
+    
 
-    for y in range(boardy):
-        for x in range(boardx):
-            if x % (cellwidth + 1) == 0:
-                region.add_element(
-                    Text(
-                        vertical,
-                        Position(
-                            region.size.half().x - floor(cellwidth / 2) - 2 + x,
-                            region.size.half().y - y + 1,
-                        ),
-                    )
-                )
-
-            if y % (cellheight + 1) == 0:
-                text = horizontal * (boardx + cellwidth)
-                new = []
-
-                for i, char in enumerate(text):
-                    new.append(char)
-                    if (i + 1) % (cellwidth + 1) == 0:
-                        new[i] = cross
-
-                new = "".join(text)
-
-                region.add_element(
-                    Text(
-                        new,
-                        Position(
-                            region.size.half().x - floor(len(text) / 2) - 1,
-                            region.size.half().y - y + 1,
-                        ),
-                    )
-                )
-
-    ui.add_region(region)
+    ui.add_region(mainRegion)
 
     ui.activate()
 
